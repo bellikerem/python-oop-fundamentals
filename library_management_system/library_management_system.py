4353d8ca9b21452570973fcbba189a33d2292d3c
Kerem Can Belli <k.belli.eng@gmail.com>
2025-10-18 15:31:20 +0300

    FEAT(Kitap): Kapsülleme prensibi uygulandı. Kitabın durumunu yöneten borrow() ve get_refund() metotları eklendi.

--- library_management_system/library_management_system.py
+++ library_management_system/library_management_system.py
@@ -1,6 +1,6 @@
 import random
 
-class book:
+class Book:
     def __init__(self, name, writer):
         self.name = name
         self.duration = "available"
@@ -9,6 +9,24 @@ class book:
     def ISBN_Calculator(self):
         return str(random.randint(1000000000000,9999999999999))
     def show_information(self):
-        pass
+        print(f"Book Name: {self.name} /// Writer Name: {self.writer} /// Duration: {self.duration} /// ISBN: {self.ISBN}")
+    def borrow(self):
+        if self.duration == "available":
+            self.duration = "borrowed"
+        else:
+            print("This book has already been borrowed.")
+    def get_refund(self):
+        self.duration = "available"
 
-    
+
+b1 = Book("Suç ve Ceza", "Dostoyevski")
+
+b1.show_information()
+
+b1.borrow()
+
+b1.show_information()
+
+b1.get_refund()
+
+b1.show_information()
