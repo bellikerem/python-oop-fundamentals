f195287f6b433ddb305bce962e1360ee6da6a7d0
Kerem Can Belli <k.belli.eng@gmail.com>
2025-10-20 21:12:45 +0300

    FEAT: Proje tamamlandı. Kalıtım ve çok biçimlilik (4 şekil) CLI arayüzü ve hata yönetimi ile birleştirildi.

--- shape_calculator/shape_calculator.py
+++ shape_calculator/shape_calculator.py
@@ -4,8 +4,10 @@ class Shape:
     def __init__ (self, name):
         self.name = name
     def area_calculator(self):
+        print("\n--- RESULT ---")
         raise NotImplementedError("This method should be implemented in child classes according to their own formula.")
     def perimeter_calculator(self):
+        print("\n--- RESULT ---")
         raise NotImplementedError("This method should be implemented in child classes according to their own formula.")
 
 class Circle(Shape):
@@ -46,3 +48,108 @@ class Triangle(Shape):
         self.base_edge = base_edge
     def area_calculator(self):
         return (self.height * self.base_edge) / 2
+    def perimeter_calculator(self):
+        print("\n--- RESULT ---")
+        raise NotImplementedError("The perimeter cannot be calculated because the edges are unknown.")
+    
+
+def check_integer(number):
+    while True:
+        try:
+            integer_input = int(input(number))
+            if integer_input > 0:
+                return integer_input
+            else:
+                print("\n--- RESULT ---")
+                print("Please input a positive value.")
+                continue
+        except ValueError:
+            print("\n--- RESULT ---")
+            print("Please input an integer")
+
+
+print("\n--- SHAPE CALCULATOR ---")
+while True:
+    try:
+        print("\n--- DECİSİON ---")
+        decision_input = input("Press 1 to calculate area\n" \
+        "Press 2 to calculate perimeter: ")
+        decision = int(decision_input)
+        if decision not in [1,2]:
+            print("\n--- RESULT ---")
+            print("Please input 1 or 2.")
+        elif decision == 1:
+            decision1 = "Area"
+            break
+        else:
+            decision1 = "Perimeter"
+            break
+    except ValueError:
+        print("\n--- RESULT ---")
+        print("Please input an integer")
+
+while True:
+    try:
+        print("\n--- MAIN MENU ---")
+        mainmenu_input = input("Press 1 if your shape is circle\n" \
+        "Press 2 if your shape is Square\n" \
+        "Press 3 if your shape is rectangle\n" \
+        "Press 4 if your shape is triangle\n" \
+        "Press 5 to quit: ")
+        mainmenu = int(mainmenu_input)
+        if mainmenu not in [1,2,3,4,5]:
+            print("\n--- RESULT ---")
+            print("Please input a number that you see on the screen")
+        elif mainmenu == 1:
+            if decision1 == "Area":
+                radius = check_integer("Please input a radius value: ")
+                circle = Circle(radius)
+                print("\n--- RESULT ---")
+                print(circle.area_calculator())
+            else:
+                radius = check_integer("Please input a radius value: ")
+                circle = Circle(radius)
+                print("\n--- RESULT ---")
+                print(circle.perimeter_calculator())
+        elif mainmenu == 2:
+            if decision1 == "Area":
+                edge = check_integer("Please input a edge value: ")
+                square = Square(edge)
+                print("\n--- RESULT ---")
+                print(square.area_calculator())
+            else:
+                edge = check_integer("Please input a edge value: ")
+                square = Square(edge)
+                print("\n--- RESULT ---")
+                print(square.perimeter_calculator())
+        elif mainmenu == 3:
+            if decision1 == "Area":
+                long_edge = check_integer("Please input a long edge value: ")
+                short_edge = check_integer("Please input a short edge value: ")
+                rectangle = Rectangle(long_edge, short_edge)
+                print("\n--- RESULT ---")
+                print(rectangle.area_calculator())
+            else:
+                long_edge = check_integer("Please input a long edge value: ")
+                short_edge = check_integer("Please input a short edge value: ")
+                rectangle = Rectangle(long_edge, short_edge)
+                print("\n--- RESULT ---")
+                print(rectangle.perimeter_calculator())
+        elif mainmenu == 4:
+            if decision1 == "Area":
+                height = check_integer("Please input a height value: ")
+                base_edge = check_integer("Please input a base edge value: ")
+                triangle = Triangle(height, base_edge)
+                print("\n--- RESULT ---")
+                print(triangle.area_calculator())
+            else:
+                height = check_integer("Please input a height value: ")
+                base_edge = check_integer("Please input a base edge value: ")
+                triangle = Triangle(height, base_edge)
+                print("\n--- RESULT ---")
+                print(triangle.perimeter_calculator())
+        else:
+            break
+    except ValueError:
+        print("Please input an integer")
+SystemExit
