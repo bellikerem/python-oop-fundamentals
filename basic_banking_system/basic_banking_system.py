71edb3c53b9adf0426508e6e72896059bb4d62fc
Kerem Can Belli <k.belli.eng@gmail.com>
2025-10-20 18:41:41 +0300

    FEAT: Bankacılık sistemi tamamlandı. kapsülleme (gizli bakiye) ve tam işlevsel CLI arayüzü eklendi REFACTOR: String ve tam sayıları kontrol eden yardımcı fonksiyonlar eklendi.

--- basic_banking_system/basic_banking_system.py
+++ basic_banking_system/basic_banking_system.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 class Account:
     def __init__(self, name, balance=0):
@@ -9,29 +10,82 @@ class Account:
         return str(random.randint(100000000,999999999))
     def withdraw_money(self, amount):
         if amount > self.__balance:
+            print("\n--- RESULT ---")
             print("Your balance is not enough for this action")
         else:
             self.__balance = self.__balance - amount
+            print("\n--- RESULT ---")
             print(f"Successfull. Your new balance is {self.__balance}")
     def deposit_money(self, amount):
         self.__balance = self.__balance + amount
+        print("\n--- RESULT ---")
         print(f"Successfull. Your new balance is {self.__balance}")
     def show_information(self):
+        print("\n--- RESULT ---")
         print(f"Account name is: {self.name} /// ID: {self.id} /// balance: {self.__balance}")
 
+def check_string(prompt):
+    while True:
+        string_input = input(prompt)
+        if not string_input.strip():
+            print("\n--- RESULT ---")
+            print("Please do not leave blank.")
+            continue
+        
+        if string_input.isdigit():
+            print("\n--- RESULT ---")
+            print("The name cannot consist of numbers only. Please enter a name.")
+            continue
 
-a1 = Account("Kerem Can Belli", 10000)
+        return string_input.strip()
 
-a1.show_information()
+def check_integer(number):
+    while True:
+        try:
+            integer_input = int(input(number))
+            if integer_input > 0:
+                return integer_input
+            elif integer_input < 0:
+                print("\n--- RESULT ---")
+                print("Please input a positive number")
+                continue
+            else:
+                print("\n--- RESULT ---")
+                print("Please do not input 0")
+                continue
+        except ValueError:
+            print("\n--- RESULT ---")
+            print("Please input an integer")
 
-a1.withdraw_money(100000)
 
-a1.show_information()
+while True:
+    print("\n--- WELCOME TO THE OOP BANK ---")
+    print("Please create an account...")
+    name = check_string("Please input an account name: ")
+    balance = check_integer("Please input a balance (if you dont input a value. it will be automaticly 0): ")
+    a1 = Account(name, balance)
+    print("Account has been created.")
+    a1.show_information()
+    break
 
-a1.withdraw_money(1)
-
-a1.show_information()
-
-a1.deposit_money(10)
-
-a1.show_information()
+while True:
+    try:
+        print("\n--- MAIN MENU ---")
+        mainmenu_input = input("Press 1 to withdraw money\n" \
+        "Press 2 to deposit money\n" \
+        "Press 3 to show information: ")
+        mainmenu = int(mainmenu_input)
+        if mainmenu not in [1,2,3]:
+            print("\n--- RESULT ---")
+            print("Please input a number that you see on the screen")
+        elif mainmenu == 1:
+            amount = check_integer("Please input an amount: ")
+            a1.withdraw_money(amount)
+        elif mainmenu == 2:
+            amount = check_integer("Please input an amount: ")
+            a1.deposit_money(amount)
+        else:
+            a1.show_information()
+    except ValueError:
+        print("\n--- RESULT ---")
+        print("Please input an integer")
